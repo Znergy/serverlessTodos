7bf7fe8ab4109be5372e6cce9a19ea60f228bc31
znergy <ryanjonesirl@gmail.com>
2017-09-10 19:07:13 -0700

    add minor changes

--- helpers/getEndpoint.js
+++ helpers/getEndpoint.js
@@ -1,4 +1,9 @@
-fs = require('fs')
+// When serverless deploy runs the command 'tee' will copy the output from
+// 'serverless deploy' and put in the file called 'deploy.out', this allows
+// getEndpoint to scan that file for the endpoint created by serverless deploy
+// Command: serverless deploy | tee deploy.out
+
+fs = require('fs');
 fs.readFile(__dirname + '/../.build/deploy.out', 'utf8', function (err,data) {
   if (err)
     throw (err);
